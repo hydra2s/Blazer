8cd8d829b1e3fa06116610267101fe852b4f0cf1
unit-a-user <helixd.2s18@gmail.com>
2023-03-16 00:34:27 +0700

    optimize usage

--- blazer.js
+++ blazer.js
@@ -5,23 +5,7 @@ export class Blazer {
 
     async init() {
         this._module = await import("./build/release.js");
-        return this;
-    }
-
-    async encode(ctx, w, h) {
-        let idata          = ctx.getImageData(0, 0, w, h),
-            stride         = ((32 * w + 31) / 32) << 2,
-            pixelArraySize = stride * h,
-            fileLength     = 122 + pixelArraySize;
-
-        //
-        let _module = this._module;
-        let _from = _module.alloc(fileLength+2)+2;
-        let _idat = _from + 122;
-        let _id32 = new Uint32Array(idata.data.buffer);
-        let _pt32 = new Uint32Array(_module.memory.buffer, _from + 122, idata.data.length>>2); _pt32.set(_id32);
-        //new Uint8Array(_module.memory.buffer, _from + 122, idata.data.length).set(idata.data);
-        const header = new Uint8Array([ // TODO: encode as UINT32
+        this._header = new Uint8Array([ // TODO: encode as UINT32
             0x42, 0x4d,             // (0) BM
             0x00, 0x00, 0x00, 0x00, // (2) total length
             0x00, 0x00, 0x00, 0x00, // (6) skip unused fields
@@ -29,8 +13,8 @@ export class Blazer {
             0x6c, 0x00, 0x00, 0x00, // (14) header size (108)
             0x00, 0x00, 0x00, 0x00, // (18) width
             0x00, 0x00, 0x00, 0x00, // (22) height
-               1, 0x00,   32, 0x00, // (26) 1 plane, 32-bits (RGBA)
-               3, 0x00, 0x00, 0x00, // (30) no compression (BI_BITFIELDS, 3)
+            1, 0x00,   32, 0x00, // (26) 1 plane, 32-bits (RGBA)
+            3, 0x00, 0x00, 0x00, // (30) no compression (BI_BITFIELDS, 3)
             0x00, 0x00, 0x00, 0x00, // (34) bitmap size incl. padding (stride x height)
             0x13, 0xB , 0x00, 0x00, // (38) pixels/meter h (~72 DPI x 39.3701 inch/m)
             0x13, 0xB , 0x00, 0x00, // (42) pixels/meter v
@@ -42,21 +26,36 @@ export class Blazer {
             0x00, 0x00, 0x00, 0xFF, // (66) alpha channel mask
             0x20, 0x6e, 0x69, 0x57  // (70) " win" color space
         ]);
+        return this;
+    }
+
+    async context(ctx, w, h) {
+        this.ctx = ctx, this.w = w, this.h = h;
+        let stride         = ((32 * w + 31) / 32) << 2,
+            pixelArraySize = stride * h;
 
         //
-        let _view = new DataView(header.buffer);
-        _view.setUint32(2, fileLength, true);
+        this.fileLength = 122 + pixelArraySize;
+        this._from = this._module.alloc(this.fileLength+2)+2;
+        new Uint8Array(this._module.memory.buffer, this._from, 122).set(this._header);
+
+        //
+        let _view = new DataView(this._module.memory.buffer, this._from, 122);
+        _view.setUint32(2, this.fileLength, true);
         _view.setUint32(18, w, true);
         _view.setUint32(22, -h >>> 0, true);
         _view.setUint32(34, pixelArraySize, true);
 
         //
-        new Uint8Array(_module.memory.buffer, _from, 122).set(header);
+        return this;
+    }
 
-        //
-        await _module.makeBGR(_idat, w, h);
-
-        //
-        return new Blob([new Uint8Array(_module.memory.buffer, _from, fileLength)], {type: "image/bmp"});
+    // for real-time animations support
+    async record(encodeBGR=true) {
+        let idata = this.ctx.getImageData(0, 0, this.w, this.h);
+        let _idat = this._from + 122;
+        new Uint32Array(this._module.memory.buffer, _idat, idata.data.length>>2).set(new Uint32Array(idata.data.buffer));
+        if (encodeBGR) this._module.makeBGR(_idat, this.w, this.h);
+        return new Blob([new Uint8Array(this._module.memory.buffer, this._from, this.fileLength)], {type: "image/bmp"});
     }
 }
