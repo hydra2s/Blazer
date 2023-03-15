b0903073ffbef31021080b75f11b201f3a699798
unit-a-user <helixd.2s18@gmail.com>
2023-03-16 00:06:44 +0700

    WIP better header writing

--- blazer.js
+++ blazer.js
@@ -40,33 +40,40 @@ export class Blazer {
         let _from = _module.alloc(fileLength+2)+2;
         let _idat = _from + 122;
         let _id32 = new Uint32Array(idata.data.buffer);
-        let _pt32 = new Uint32Array(_module.memory.buffer, _from + 122, idata.data.length>>2);
-        _pt32.set(_id32);
+        let _pt32 = new Uint32Array(_module.memory.buffer, _from + 122, idata.data.length>>2); _pt32.set(_id32);
         //new Uint8Array(_module.memory.buffer, _from + 122, idata.data.length).set(idata.data);
-        let writer = new FileWriter(_module.memory.buffer, _from, fileLength);
+          let writer = new FileWriter(_module.memory.buffer, _from, fileLength);
+        const header = new Uint8Array([ // TODO: encode as UINT32
+            0x42, 0x4d,             // (0) BM
+            0x00, 0x00, 0x00, 0x00, // (2) total length
+            0x00, 0x00, 0x00, 0x00, // (6) skip unused fields
+            0x7a, 0x00, 0x00, 0x00, // (10) offset to pixels
+            0x6c, 0x00, 0x00, 0x00, // (14) header size (108)
+            0x00, 0x00, 0x00, 0x00, // (18) width
+            0x00, 0x00, 0x00, 0x00, // (22) height
+               1, 0x00,   32, 0x00, // (26) 1 plane, 32-bits (RGBA)
+               3, 0x00, 0x00, 0x00, // (30) no compression (BI_BITFIELDS, 3)
+            0x00, 0x00, 0x00, 0x00, // (34) bitmap size incl. padding (stride x height)
+            0x13, 0xB , 0x00, 0x00, // (38) pixels/meter h (~72 DPI x 39.3701 inch/m)
+            0x13, 0xB , 0x00, 0x00, // (42) pixels/meter v
+            0x00, 0x00, 0x00, 0x00, // (46) skip color/important colors
+            0x00, 0x00, 0x00, 0x00, // (50) skip color/important colors
+            0x00, 0x00, 0xFF, 0x00, // (54) red channel mask
+            0x00, 0xFF, 0x00, 0x00, // (60) green channel mask
+            0xFF, 0x00, 0x00, 0x00, // (64) blue channel mask
+            0x00, 0x00, 0x00, 0xFF, // (66) alpha channel mask
+            0x20, 0x6e, 0x69, 0x57  // (70) " win" color space
+        ]);
 
-        // Header
-        writer.set16(0x4d42);									// BM
-        writer.set32(writer.view.byteLength); 					// total length
-        writer.seek(4);											// skip unused fields
-        writer.set32(0x7a);										// offset to pixels
+        //
+        let _view = new DataView(header.buffer);
+        _view.setUint32(2, writer.view.byteLength, true);
+        _view.setUint32(18, w, true);
+        _view.setUint32(22, -h >>> 0, true);
+        _view.setUint32(34, pixelArraySize, true);
 
-        // DIB header
-        writer.set32(0x6c);										// header size (108)
-        writer.set32(w);
-        writer.set32(-h >>> 0);									// negative = top-to-bottom
-        writer.set16(1);										// 1 plane
-        writer.set16(32);										// 32-bits (RGBA)
-        writer.set32(3);										// no compression (BI_BITFIELDS, 3)
-        writer.set32(pixelArraySize);							// bitmap size incl. padding (stride x height)
-        writer.set32(2835);										// pixels/meter h (~72 DPI x 39.3701 inch/m)
-        writer.set32(2835);										// pixels/meter v
-        writer.seek(8);											// skip color/important colors
-        writer.set32(0xff0000);									// red channel mask
-        writer.set32(0xff00);									// green channel mask
-        writer.set32(0xff);										// blue channel mask
-        writer.set32(0xff000000);								// alpha channel mask
-        writer.set32(0x57696e20);								// " win" color space
+        //
+        new Uint8Array(_module.memory.buffer, _from, 122).set(header);
 
         //
         await _module.makeBGR(_idat, w, h);
