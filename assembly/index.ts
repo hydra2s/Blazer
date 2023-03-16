5ec530915d5ea6dcaa5e6b7a84e5cdbc772322ba
unit-a-user <helixd.2s18@gmail.com>
2023-03-16 17:07:12 +0700

    correct adler32

--- assembly/index.ts
+++ assembly/index.ts
@@ -13,13 +13,13 @@ export function makeARGB(fileData: usize, width: u32, height: u32): void {
 
 export function makePNGData(fileData: usize, width: u32, height: u32): void {
   let W: u32 = width*4;
-  let a: u16 = 1, b: u32 = 0;
+  let a: u32 = 1, b: u32 = 0;
   const REM: u16 = 65521;
   for (let Y:u32=0;Y<height;Y++) {
-    let P: usize = fileData+Y*(W+6); 
+    let P: usize = fileData+Y*(W+6);
     store<u8>(0+P, Y==height-1?1:0);
-    store<u16>(1+P, (W+1));
-    store<u16>(3+P, ~(W+1));
+    store<u16>(1+P, <u16>W+1);
+    store<u16>(3+P, ~(<u16>W+1));
 
     // lane with filter 0
     store<u8>(5+P, 0); getPixelPack(0,Y,width,1,6+P);
@@ -32,7 +32,7 @@ export function makePNGData(fileData: usize, width: u32, height: u32): void {
       //v128.store(pX, v128.swizzle(v128.load(pX), i8x16(3,0,1,2, 7,4,5,6, 11,8,9,10, 15,12,13,14)));
     }
   }
-  store<u32>(height*(W+6), ((b << 16) | a));
+  store<u32>(fileData+height*(W+6), bswap<u32>(((b << 16) | a)));
 }
 
 export function alloc(size: u32): usize {
