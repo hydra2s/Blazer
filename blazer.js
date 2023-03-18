26796c4bccf0202593b46650cb833a2e0297cc8e
unit-a-user <helixd.2s18@gmail.com>
2023-03-18 16:24:26 +0700

    add IDLE for based worker libraries

--- blazer.js
+++ blazer.js
@@ -4,6 +4,37 @@ import { BlazerBMP } from "./lib/bmp.js"
 import { DataReader } from "./lib/reader.js"
 
 //
+import * as utils from "./lib/utils.js"
+
+//
 export { OpenJNG, BlazerPNG, BlazerBMP, DataReader };
 export * from "./lib/utils.js"
 export * from "./lib/intercom.js"
+
+//
+import { InterWork } from "./lib/intercom.js"
+
+//
+const WorkerLib = {};
+
+//
+if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
+    const WC = new InterWork(self); WorkerLib.interwork = WC;
+    const _module = {
+        OpenJNG, BlazerPNG, BlazerBMP, OffscreenCanvas, WorkerLib, 
+        ...utils
+    };
+
+    // will used for await ops (such as register)
+    WC.register("idle", WorkerLib.idle = async()=> {
+        return await new Promise(R=>R(_module));
+    });
+
+    //
+    WC.instance(OffscreenCanvasRenderingContext2D, OffscreenCanvas, OpenJNG, BlazerPNG, BlazerBMP);
+    WC.register("default", WorkerLib.library = _module);
+    WC.observe();
+}
+
+//
+export { WorkerLib };
