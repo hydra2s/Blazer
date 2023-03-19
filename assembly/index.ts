18eb9ade7af34e40c25a83b14e7dec40d03c15fd
unit-a-user <helixd.2s18@gmail.com>
2023-03-19 21:07:02 +0700

    prefer RGBA BMP format

--- assembly/index.ts
+++ assembly/index.ts
@@ -11,6 +11,17 @@ export function makeARGB(fileData: usize, width: u32, height: u32): void {
   }
 }
 
+export function makeRGBA(fileData: usize, width: u32, height: u32): void {
+  let W: u32 = width*4;
+  for (let Y:u32=0;Y<height;Y++) {
+    let P: usize = getPixelPack(0,Y,width,1,fileData+Y*W);
+    //for (let X:u32=0;X<W;X+=16) {
+      //let pX: usize = P+X;
+      //v128.store(pX, v128.swizzle(v128.load(pX), i8x16(3,0,1,2, 7,4,5,6, 11,8,9,10, 15,12,13,14)));
+    //}
+  }
+}
+
 //
 function i32x4_dot_i8x16(a: v128, b: v128): v128 {
   return i32x4.add(
