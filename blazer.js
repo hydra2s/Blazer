e2aa49e74a68b7e030bf7204e7ed802514b1c792
unit-a-user <helixd.2s18@gmail.com>
2023-07-01 08:14:08 +0700

    WIP specific custom elements

--- blazer.js
+++ blazer.js
@@ -243,6 +243,266 @@ if (!(typeof self != "undefined" && typeof WorkerGlobalScope !== 'undefined' && 
         }, { extends: "source" });
     } catch(e) {};
 
+    try {
+        customElements.define("scroll-able", class Scrollable extends HTMLElement {
+            constructor() {
+                super();
+
+                //
+                this._scrollX = document.createElement("div");
+                this._scrollY = document.createElement("div");
+                this._scrollable = document.createElement("div");
+                this._slot = document.createElement("slot");
+                this._style = document.createElement("style");
+
+                //
+                this._scrollX.classList.add("scroll-x");
+                this._scrollY.classList.add("scroll-y");
+                this._scrollable.classList.add("scrollable");
+
+                //
+                this.attachShadow({ mode: "open" });
+                this.shadowRoot.appendChild(this._style);
+                this.shadowRoot.appendChild(this._scrollX);
+                this.shadowRoot.appendChild(this._scrollY);
+                this.shadowRoot.appendChild(this._scrollable);
+
+                //
+                this._scrollable.appendChild(this._slot);
+                this._style.textContent = `
+                /* TODO: scrolling library! */
+                :host {
+                    width: min(100%, 100vw);
+                    height: min(100%, 100vh);
+
+                    max-width: min(100%, 100vw);
+                    max-height: min(100%, 100vh);
+
+                    display: inline-block;
+                    position: relative;
+                    overflow: hidden;
+
+                    padding: 0px;
+                    margin: 0px;
+                }
+
+                .scrollable {
+                    display: block;
+                    text-align: center;
+                    position: relative;
+                
+                    /* */
+                    width: min(100%, 100vw);
+                    max-width: min(100%, 100vw);
+                    min-height: min(100vh, 100%);
+                    max-height: min(100vh, 100%);
+                    height: min(100vh, 100%);
+                
+                    height: max-content;
+                    position: absolute;
+                    left: 0; top: 0; right: 0; bottom: 0;
+                
+                    /* */
+                    overflow: auto;
+                    overflow: overlay;
+                    overflow-x: hidden;
+                    touch-action: none;
+                
+                    /* */
+                    scrollbar-gutter: stable both-edges;
+                    scrollbar-width: thin;
+                    scrollbar-width: none;
+                    scroll-behavior: smooth;
+                }
+                
+                /* */
+                .scroll-y {
+                    position: absolute;
+                    height: calc(100% - 1rem);
+                    width: 1rem;
+                    
+                    top: 0px;
+                
+                    right: 0px;
+                    left: auto;
+                    background-color: transparent;
+                }
+                
+                /* */
+                .scroll-x {
+                    position: absolute;
+                    width: calc(100% - 1rem);
+                    height: 1rem;
+                    
+                    left: 0px;
+                
+                    bottom: 0px;
+                    top: auto;
+                    background-color: transparent;
+                }
+                
+                /* */
+                .scroll-x .track, .scroll-x .track {
+                    /* important! */
+                    position: sticky;
+                }
+`;
+
+                //
+                return this;
+            }
+
+            disconnectedCallback() {
+                
+            }
+
+            connectedCallback() {
+                
+            }
+
+            attributeChangedCallback(name, oldValue, newValue) {
+                
+            }
+
+            static get observedAttributes() {
+                return [];
+            }
+        });
+    } catch(e) {};
+
+
+
+    try {
+        customElements.define("slider-x", class SliderX extends HTMLElement {
+            constructor() {
+                super();
+
+                //
+                this._scrollX = document.createElement("div");
+                this._scrollY = document.createElement("div");
+                this._scrollable = document.createElement("div");
+                this._slot = document.createElement("slot");
+                this._style = document.createElement("style");
+
+                //
+                this._scrollX.classList.add("scroll-x");
+                this._scrollY.classList.add("scroll-y");
+                this._scrollable.classList.add("scrollable");
+
+                const E = this._scrollable;
+                E.addEventListener("wheel", (e)=>{
+                    e.preventDefault();
+    
+                    // abuse a scrolling bug?
+                    if (e.deltaY > 0) { E.scrollBy( 10, 0); };
+                    if (e.deltaY < 0) { E.scrollBy(-10, 0); };
+                });
+
+                //
+                this.attachShadow({ mode: "open" });
+                this.shadowRoot.appendChild(this._style);
+                this.shadowRoot.appendChild(this._scrollX);
+                this.shadowRoot.appendChild(this._scrollY);
+                this.shadowRoot.appendChild(this._scrollable);
+
+                //
+                this._scrollable.appendChild(this._slot);
+                this._style.textContent = `
+                /* TODO: scrolling library! */
+                :host {
+                    width: min(100%, 100vw);
+                    height: min(100%, 100vh);
+
+                    max-width: min(100%, 100vw);
+                    max-height: min(100%, 100vh);
+
+                    display: inline-block;
+                    position: relative;
+                    overflow: hidden;
+
+                    padding: 0px;
+                    margin: 0px;
+                }
+
+                .scrollable > ::slotted(*) {
+                    flex: 0 0 100%;
+                    aspect-ratio: 16 / 9;
+                
+                    max-height: 100%;
+                    max-width: 100%;
+                    width: 100%;
+
+                    left: 0px;
+                    top: 0px;
+                    bottom: 0px;
+                    right: 0px;
+                    display: inline-block;
+                    overflow: hidden;
+                
+                    /* */
+                    margin: 0;
+                    padding: 0;
+                
+                    /* */
+                    scroll-snap-align: center;
+                }
+                
+                .scrollable {
+                    height: max-content;
+                    display: inline-flex;
+                    flex-direction: row;
+                
+                    /* use overlay scroll-bars library */
+                    scrollbar-gutter: stable both-edges;
+                    scrollbar-width: thin;
+                    scrollbar-width: none;
+                    scroll-behavior: smooth;
+                
+                    /* */
+                    overflow: hidden;
+                    overflow-y: hidden;
+                    overflow-x: scroll;
+                
+                    /* */
+                    scroll-snap-type: x mandatory;
+                    scroll-margin-left: 0;
+                
+                    /* */
+                    touch-action: pan-x;
+                
+                    /* */
+                    margin: 0;
+                    padding: 0;
+                }
+
+`;
+
+                //
+                return this;
+            }
+
+            disconnectedCallback() {
+                
+            }
+
+            connectedCallback() {
+                
+            }
+
+            attributeChangedCallback(name, oldValue, newValue) {
+                
+            }
+
+            static get observedAttributes() {
+                return [];
+            }
+        });
+    } catch(e) {};
+
+
+
+
+
 }
 
 export { JNGImage };
