6b1438f6751228084b7c22bd7ebebc015222d676
unit-a-user <helixd.2s18@gmail.com>
2023-07-08 16:18:10 +0700

    trying to load by such way

--- magick-jng.mjs
+++ magick-jng.mjs
@@ -1,5 +1,5 @@
  // Ha-ha, JNG can be opended in browser by such way...
- import {
+import {
     initializeImageMagick,
     ImageMagick,
     Magick,
@@ -7,7 +7,18 @@
     Quantum,
 } from './@imagemagick/magick-wasm/dist';
 
-initializeImageMagick("./@imagemagick/magick-wasm/dist/magick.wasm").then(async() => {
+//
+const _CS_ = typeof document != "undefined" ? document.currentScript : {};
+const _PATH_ = ((((_CS_ ? (_CS_.src||"") : (import.meta.url||"")).split('?')[0]||"")||"").split('/').slice(0, -1).join('/')+'/')||"";
+const _loader_ = async(url)=>{
+    try {
+        const _txt_ = (await import(url + "?url"))?.default;
+        if (_txt_) { return _txt_; };
+    } catch(e) {};
+    return URL.createObjectURL(await (await fetch(_PATH_ + url)).blob());
+}
+
+_loader_("./@imagemagick/magick-wasm/dist/magick.wasm").then((url)=>initializeImageMagick(url).then(async() => {
     console.log(Magick.imageMagickVersion);
     console.log('Delegates:', Magick.delegates);
     console.log('Features:', Magick.features);
@@ -20,4 +31,4 @@ initializeImageMagick("./@imagemagick/magick-wasm/dist/magick.wasm").then(async(
     })
 }).catch(err => {
     console.error(err);
-});
+}));
