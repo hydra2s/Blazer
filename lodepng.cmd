3fa4c0af8ed84ef7b5a2da5ee0d5dac35007ff10
unit-a-user <helixd.2s18@gmail.com>
2023-07-10 08:55:48 +0700

    single file

--- lodepng.cmd
+++ lodepng.cmd
@@ -1,1 +1,2 @@
-emcc lodepng.cpp  -msimd128 -Oz -O3 -s LINKABLE=1 -s EXPORT_ALL=1 -sMODULARIZE -sEXPORTED_RUNTIME_METHODS="ccall,cwrap" -s ALLOW_MEMORY_GROWTH=1 -s EXPORT_ES6=1 -s TOTAL_MEMORY=1024MB -s EXPORTED_FUNCTIONS="['_malloc', '_free', '_calloc']"
+ emcc lodepng.cpp  -msimd128 -Oz -O3 -s LINKABLE=1 -s EXPORT_ALL=1 -sMODULARIZE -sEXPORTED_RUNTIME_METHODS="ccall,cwrap" -s ALLOW_MEMORY_GROWTH=1 -s EXPORT_ES6=1 -s TOTAL_MEMORY=1024MB -s EXPORTED_FUNCTIONS="['_malloc', '_free', '_calloc']" -sSINGLE_FILE -o lodepng.js
+ 
